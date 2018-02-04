31a60a5bd982835af00aadf2e7ee4df015498bfa
Ru Chern Chong <me@ruchern.com>
2018-02-05 02:58:27 +0800

    Add the main code of the bot
    Signed-off-by: Ru Chern Chong <me@ruchern.com>

---
+++ index.js
@@ -0,0 +1,19 @@
+import keys from './keys.js'
+
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const token = keys.token
+
+client.on('ready', () => {
+    console.log('I am ready!')
+})
+
+client.on('guildMemberAdd,' member => {
+    const channel = member.guild.channels.find('general', 'general')
+
+    if (!channel) return
+
+    channel.send(`Welcome ${member} to Cryptex. Enjoy your stay!`)
+})
+
+client.login(token)
