9b6f7b5c233d7ba9980c2fd35c7a3b0d140ffbef
Ru Chern Chong <me@ruchern.com>
2018-02-05 04:22:23 +0800

    Move index.js into src folder
    Signed-off-by: Ru Chern Chong <me@ruchern.com>

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
-const { token } = require('./keys')
+const { token } = require('../keys')
 
 client.on('ready', () => {
   console.log('I am ready!')
