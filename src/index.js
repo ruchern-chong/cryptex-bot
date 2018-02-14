12ebfebe4aa46848219a7a863e503ee218ca6dfb
Ru Chern Chong <me@ruchern.com>
2018-02-15 02:27:53 +0800

    Update welcome message
    Signed-off-by: Ru Chern Chong <me@ruchern.com>

--- src/index.js
+++ src/index.js
@@ -13,11 +13,9 @@ client.on('guildMemberAdd', member => {
 
     if (!channel) return
 
-    channel.send(`
-    Welcome ${member} to Cryptex.
-    You may start by reading the rules of this server in ${member.guild.channels.find('name', 'welcome-and-faq')}.
-    Trading chat on crypto currencies are held in #trading-chat and #trading-voice-text.
-    You could also get a good technical analysis on some of the coins or tokens posted by our qualified analysists in #analysis. Do keep a look out for people with the \`Analyst\` role. You could also add the coins or tokens that you are following from teh #commands channel.
+    channel.send(`Welcome ${member} to Cryptex. You may start by reading the rules of this server in ${member.guild.channels.find('name', 'welcome-and-faq')}.
+    Trading chat on crypto currencies are held in ${member.guild.channels.find('name', 'trading-chat')} and ${member.guild.channels.find('name', 'trading-voice-text')}.
+    You could also get a good technical analysis on some of the coins or tokens posted by our qualified analysts in ${member.guild.channels.find('name', 'analysis')}. Do keep a look out for people with the \`Analyst\` role. You could also add the coins or tokens that you are following from the ${member.guild.channels.find('name', 'commands')} channel.
     Last but not least, this server is all about sharing. Do note that this server has very strict rules on sharing links with referral links. If you wish to really share something awesome, do remove the referral links before sharing them.
     Thank you and welcome to Cryptex once again. I hope you enjoy your stay!
   `)
