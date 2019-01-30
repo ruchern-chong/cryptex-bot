8d0d7a9a3c11607f8320344ca3e4f076dcb2fcfc
Ru Chern Chong <hello@ruchern.net>
2019-01-30 22:51:13 +0800

    Remove config.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-const Discord = require('discord.js')
+import Discord from 'discord.js'
+import { token } from '../keys'
+
 const client = new Discord.Client()
-const { token } = require('./../keys')
 
 client.on('ready', () => console.log('Client is ready'))
 
 client.on('message', async message => {
   const { member, channel, guild } = message
 
-  if (message.content === '!invites') {
+  if (message.content === `!invites`) {
     const stripEmoji = /([\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2694-\u2697]|\uD83E[\uDD10-\uDD5D])/g
 
     const inviteRoles = [
@@ -20,17 +21,26 @@ client.on('message', async message => {
     let invites, userInvites
     let inviteCount = 0
 
-    await guild.fetchInvites().then(response => invites = response).catch(error => console.log(error))
+    await guild
+      .fetchInvites()
+      .then(response => (invites = response))
+      .catch(error => console.log(error))
 
     userInvites = invites.filter(invite => invite.inviter === member.user)
 
-    userInvites.forEach(userInvite => inviteCount += userInvite.uses)
+    userInvites.forEach(userInvite => (inviteCount += userInvite.uses))
 
-    const roleToBeAdded = inviteRoles.find(inviteRole => inviteCount >= inviteRole.threshold)
+    const roleToBeAdded = inviteRoles.find(
+      inviteRole => inviteCount >= inviteRole.threshold
+    )
 
-    const roleId = guild.roles.find(guildRole => guildRole.name.replace(stripEmoji, '').includes(roleToBeAdded.name)).id
+    const roleId = guild.roles.find(guildRole =>
+      guildRole.name.replace(stripEmoji, '').includes(roleToBeAdded.name)
+    ).id
 
-    member.addRole(roleId).then(() => console.log('Member assigned with respective role.'))
+    member
+      .addRole(roleId)
+      .then(() => console.log('Member assigned with respective role.'))
 
     channel.send(`Hi ${member}, you have ${inviteCount} invites.`)
   }
